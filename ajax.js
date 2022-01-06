2541f0a056ebe24b90fd94d00d72e3f7c1898981
joy <joychoudhury318@gmail.com>
2022-01-06 18:15:56 +0530

    v2

--- ajax.js
+++ ajax.js
@@ -1,4 +1,33 @@
-function bubbletext(){
-    // var http = new XMLHttpRequest();
+function bubbletext() {
+    const box1 = document.querySelector('.box-1');  
+    box1.innerHTML = `<p>Bubble Sort</p>`;  
 
+    const box3 = document.querySelector('.box-3'); 
+    box3.innerHTML = `<p>Time :	O(n^2)</p>
+    <p>Space :	O(1)</p>`; 
+
+}
+function selectiontext() {
+    const box1 = document.querySelector('.box-1');  
+    box1.innerHTML = `<p>Selection Sort</p>`; 
+    
+    const box3 = document.querySelector('.box-3');  
+    box3.innerHTML = `<p>Time :	O(n^2)</p>
+    <p>Space :	O(1)</p>`;
+}
+function insertiontext() {
+    const box1 = document.querySelector('.box-1');  
+    box1.innerHTML = `<p>Insertion Sort</p>`;  
+
+    const box3 = document.querySelector('.box-3');  
+    box3.innerHTML = `<p>Time :	O(n^2)</p>
+    <p>Space :	O(1)</p>`;
+}
+function mergetext() {
+    const box1 = document.querySelector('.box-1');  
+    box1.innerHTML = `<p>Merge Sort</p>`;  
+
+    const box3 = document.querySelector('.box-3');  
+    box3.innerHTML = `<p>Time :	O(nlog n)</p>
+    <p>Space :	O(n)</p>`;
 }
--- app.js
+++ app.js
@@ -29,10 +29,17 @@ sortingMethod[1].addEventListener('click',()=>{
     bubbleSort();
     bubbletext();
 });
-sortingMethod[2].addEventListener('click',selectionSort);
-sortingMethod[3].addEventListener('click',insertionSort);
+sortingMethod[2].addEventListener('click',()=>{
+    selectionSort();
+    selectiontext()
+});
+sortingMethod[3].addEventListener('click',()=>{
+    insertionSort();
+    insertiontext()
+});
 sortingMethod[4].addEventListener('click',()=>{
     mergeSort(0,size.value-1);
+    mergetext() 
 });
 // sortingMethod[5].addEventListener('click',quickSort);
 // sortingMethod[6].addEventListener('click',heapSort);
